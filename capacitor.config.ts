0339a0e6f42c6441b82f3f0dd46a5d10d2a27aed
AbdElmoniem2003 <elmoniemabd0@gmail.com>
2025-09-11 09:00:19 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'newTasky',
+  webDir: 'www'
+};
+
+export default config;
